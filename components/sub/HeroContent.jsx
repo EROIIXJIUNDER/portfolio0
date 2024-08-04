5035168398ca1a433cba90c5c10a43bc2c2ffdc9
asmitgandu <asmitadk31@gmail.com>
2024-08-04 06:16:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/sub/HeroContent.jsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-20 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+      <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            You Can&apos;t Defeat Me At My Own Game.
+          </h1>
+        </motion.div>
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Anta&size=50&pause=1000&color=FFFFFF&random=false&width=435&height=75&lines=Hi+There!;I'm+IzanamiiDevv.;I'm+from+Phillipines!" alt="Typing SVG" /></a>
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            Web and Game
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m a Full Stack Web Developer with experience in Programming Since 2019,
+          Im a student at SanFrancisco High School. Check out my projects and skills.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
+        
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
